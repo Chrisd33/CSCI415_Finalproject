4ff4fd142b74c28df55ae571a7ce0e03f46aeeba
Chrisd33 <csdahl35@gmail.com>
2017-04-18 18:58:15 -0500

    Add files via upload

---
+++ SentimentAnalysisMapper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class SalesTestMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+   @Override
+   public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      String line = value.toString();
+      String[] fields = line.split("\t");
+      if (fields.length > 2) {
+         String store = fields[3];
+         if (store.equals("Toys") || store.equals("Consumer Electronics")) {
+            String sales = fields[4];
+            context.write(new Text(store), new Text(sales));
+         }
+      }
+   }
+}
---
+++ SentimentAnalysisReducer.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.text.DecimalFormat;
+import java.math.BigDecimal;
+
+public class SalesTestReducer extends Reducer<Text, Text, Text, Text> {
+
+   @Override
+   public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+      BigDecimal totalValue = BigDecimal.ZERO;
+      for (Text value : values) {
+         BigDecimal bigDecimalValue = new BigDecimal(value.toString());
+         totalValue = totalValue.add(bigDecimalValue);
+      }
+      Text t = new Text(totalValue.toString());
+      context.write(key, t);
+   }
+}
